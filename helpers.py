356639203bb5e2586c9950db670eecb1d4d830fd
Faizan Sana <faizansana01@gmail.com>
2022-03-24 10:27:52 -0400

    Add generate_vertices helpers function

--- helpers.py
+++ helpers.py
@@ -1,6 +1,8 @@
 import argparse
 import random
 
+import graph
+
 
 def parse_arguments():
     parser = argparse.ArgumentParser(prog='A* algorithm', description='Calculates the A* algorithm')
@@ -18,3 +20,21 @@ def generate_random_number(min: int = 0, max: int = 100):
     """
     random.seed()
     return random.randint(min, max)
+
+
+def generate_vertices(num_of_vertices: int, local_graph: graph.Graph):
+    """Generates specified number of vertices
+
+    Args:
+        num_of_vertices (int): Number of vertices
+    """
+
+    for node in range(num_of_vertices):
+        while True:
+            x_cor = generate_random_number()
+            y_cor = generate_random_number()
+
+            vertex = graph.Vertex(node, x_cor, y_cor)
+            if local_graph.check_if_vertex_exists(vertex) is False:
+                local_graph.add_vertex(node, x_loc=x_cor, y_loc=y_cor)
+                break
