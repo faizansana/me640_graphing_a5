1af407874e759026055a51eda8d030671bdadf7d
Faizan Sana <faizansana01@gmail.com>
2022-03-24 09:53:04 -0400

    Modify Vertex and Graph with cartesian coordinates

--- graph.py
+++ graph.py
@@ -1,10 +1,16 @@
+"""
+Inspired from https://www.bogotobogo.com/python/python_graph_data_structures.php
+"""
+
 from typing import Dict, Union
 
 
 class Vertex:
-    def __init__(self, node: Union[int, str]):
+    def __init__(self, node: Union[int, str], x_loc: int, y_loc: int):
         self.id = node
         self.adjacent: Dict[Union[int, str], int] = {}
+        self.x_loc = x_loc
+        self.y_loc = y_loc
 
     def __str__(self):
         return str(self.id) + ' adjacent: ' + str([x.id for x in self.adjacent])
@@ -30,9 +36,9 @@ class Graph:
     def __iter__(self):
         return iter(self.vert_dict.values())
 
-    def add_vertex(self, node: Union[int, str]):
+    def add_vertex(self, node: Union[int, str], x_loc: int, y_loc: int):
         self.num_vertices = self.num_vertices + 1
-        new_vertex = Vertex(node)
+        new_vertex = Vertex(node, x_loc, y_loc)
         self.vert_dict[node] = new_vertex
         return new_vertex
 
@@ -43,10 +49,8 @@ class Graph:
             return None
 
     def add_edge(self, frm: Union[int, str], to: Union[int, str], cost: int = 0):
-        if frm not in self.vert_dict:
-            self.add_vertex(frm)
-        if to not in self.vert_dict:
-            self.add_vertex(to)
+        if frm not in self.vert_dict or to not in self.vert_dict:
+            raise Exception('Adding edge for vertex that does not exist!')
 
         self.vert_dict[frm].add_neighbor(self.vert_dict[to], cost)
         self.vert_dict[to].add_neighbor(self.vert_dict[frm], cost)
@@ -59,12 +63,12 @@ if __name__ == '__main__':
 
     graph = Graph()
 
-    graph.add_vertex('a')
-    graph.add_vertex('b')
-    graph.add_vertex('c')
-    graph.add_vertex('d')
-    graph.add_vertex('e')
-    graph.add_vertex('f')
+    graph.add_vertex('a', 3, 1)
+    graph.add_vertex('b', 4, 1)
+    graph.add_vertex('c', 5, 6)
+    graph.add_vertex('d', 5, 1)
+    graph.add_vertex('e', 2, 2)
+    graph.add_vertex('f', 4, 5)
 
     graph.add_edge('a', 'b', 7)
     graph.add_edge('a', 'c', 9)
